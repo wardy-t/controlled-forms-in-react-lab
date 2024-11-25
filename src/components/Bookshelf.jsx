cedb72a163d75bab392bf36ac505a7f9fa2f93f7
Tom Ward <wardusmcbay@gmail.com>
2024-11-25 20:42:43 +0000

    first commit

---
+++ src/components/Bookshelf.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+<div className="bookshelfDiv">
+  <div className="formDiv">
+    <h3>Add a Book</h3>
+    {/* Form will go here */}
+  </div>
+  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+</div>
